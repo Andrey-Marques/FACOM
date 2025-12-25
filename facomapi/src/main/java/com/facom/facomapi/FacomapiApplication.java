45bc3cb9dbd99aa31b9b248e96393c53f7789624
Andrey Marques <andreymarques958@gmail.com>
2025-12-24 19:49:30 -0500

    criação do projeto

---
+++ facomapi/src/main/java/com/facom/facomapi/FacomapiApplication.java
@@ -0,0 +1,13 @@
+package com.facom.facomapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacomapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacomapiApplication.class, args);
+	}
+
+}
---
+++ facomapi/src/test/java/com/facom/facomapi/FacomapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.facom.facomapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacomapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
